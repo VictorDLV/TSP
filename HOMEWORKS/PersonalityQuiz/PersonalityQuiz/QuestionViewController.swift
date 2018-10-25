e65f2abee34e18e8fb4dd89f89557c0841c22e9e
Deivid <33154068+VictorDLV@users.noreply.github.com>
2018-10-25 12:15:04 -0500

    Add files via upload

--- HOMEWORKS/PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
+++ HOMEWORKS/PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
@@ -12,6 +12,8 @@ class QuestionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //llamada del metodo updateUI
+        updateUI()
 
         // Do any additional setup after loading the view.
     }
@@ -20,6 +22,60 @@ class QuestionViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var singleStackView: UIStackView!
+    
+    @IBOutlet weak var multipleStackView: UIStackView!
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    
+    var questionIndex = 0
+    
+    var questions: [Question] = [
+        Question(text: "Which food do you like the most?", type:.single,
+                 answers: [
+                    Answer(text: "Steak", type: .dog),
+                    Answer(text: "Fish", type: .cat),
+                    Answer(text: "Carrots", type: .rabbit),
+                    Answer(text: "Corn", type: .turtle)]),
+        
+        Question(text: "Which activities do you enjoy?", type: .multiple,
+                 answers: [
+                    Answer(text: "Swimming", type: .turtle),
+                    Answer(text: "Sleeping", type: .cat),
+                    Answer(text: "Cuddling", type: .rabbit),
+                    Answer(text: "Eating", type: .dog)]),
+        
+        Question(text: "How much do you enjoy car rides?", type: .ranged,
+                 answers: [
+                    Answer(text: "I dislike them", type: .cat),
+                    Answer(text: "I get a little nervous", type: .rabbit),
+                    Answer(text: "I barely notice them", type: .turtle),
+                    Answer(text: "I love them", type: .dog)])
+    ]
+    
+    //Creacion del metodo actualizas el UI
+    //esta funcion se encargara de cambiar el titulo de la pregunta
+    func updateUI(){
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
+        
+        navigationItem.title = "Question #\(questionIndex+1)"
+        
+        let currentQuestion = questions[questionIndex]
+        
+        switch currentQuestion.type {
+        case .single:
+            singleStackView.isHidden = false
+        case .multiple:
+            multipleStackView.isHidden = false
+        case .ranged:
+            rangedStackView.isHidden = false
+        }
+       
+    }
+   
     
 
     /*
